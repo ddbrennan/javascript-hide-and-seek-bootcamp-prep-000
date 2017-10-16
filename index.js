3f61029a239b91d4c121c4b1be53d60300824ad0
David Brennan <ddbren@gmail.com>
2017-10-16 21:59:11 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,23 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
+
+function nestedTarget() {
+  return document.querySelector('#nested .target')
+}
+
+function deepestChild() {
+  var div = document.getElementById('grand-node')
+  while (div.children[0]) {
+    div = div.children[0]
+  }
+  return div
+}
+
+function increaseRankBy(n) {
+  const lis = document.querySelectorAll('ul.ranked-list li')
+
+  for (let i = 0; i < lis.length; i++) {
+    lis[i].innerHTML = parseInt(lis[i].innerHTML) + n
+  }
+}
