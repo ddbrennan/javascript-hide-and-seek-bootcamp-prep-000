f34e251fec3b6f52bc861c79ba951e12934fd285
David Brennan <ddbren@gmail.com>
2017-10-16 21:53:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ function nestedTarget() {
 }
 
 function deepestChild() {
-  var div = document.getElementById('grand-node')
-  while (div.children) {
-    div = div.children
+  var div = document.getElementById('grand-node')[0]
+  while (div.children[0]) {
+    div = div.children[0]
   }
   return div
 }
